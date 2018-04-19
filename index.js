bb882324a86ecce948d54884bdde0d79d437c722
dshoulders <dshoulders@outlook.com>
2018-04-19 12:14:25 +0100

    CORE-4211 - Add prompts for running offline

--- index.js
+++ index.js
@@ -3,47 +3,64 @@ const progress = require('progress');
 const rp = require('request-promise-native');
 const fs = require('fs-extra');
 const mkdirp = require('mkdirp');
+const metadata = require('./metadata');
+
+const generateHtml = ({ 
+    template, makeOffline, theme, tenantId, flowId, flowVersionId, username, password
+}) => template
+    .replace('{{{OFFLINE}}}', makeOffline ? '<script src="js/ui-offline.js"></script>' : '')
+    .replace('{{{METADATA}}}', makeOffline ? '<script src="js/ui-metadata.js"></script>' : '')
+    .replace('{{{THEME}}}', theme)
+    .replace('{{{TENANT_ID}}}', tenantId)
+    .replace('{{{FLOW_ID}}}', flowId)
+    .replace('{{{FLOW_VERSION_ID}}}', flowVersionId)
+    .replace('{{{USERNAME}}}', username)
+    .replace('{{{PASSWORD}}}', password);
 
 (async function() {    
     console.log('Downloading UI Framework');
 
     const bundles = await rp.get({
-        uri: 'https://assets.manywho.com/bundles.json',
+        uri: 'https://s3.amazonaws.com/manywho-cdn-react-staging/bundles.json',
         json: true
+    });
+    
+    const theme = await promptly.prompt('Theme (defaults to paper):', { default: 'paper' });    
+    const tenantId = await promptly.prompt('Tenant Id:');
+    const flowId = await promptly.prompt('Flow Id:');
+    const flowVersionId = await promptly.prompt('Flow Version Id:', { default: '' });
+    const makeOffline = await promptly.confirm('Do you want to go offline? y/n');
+    const username = makeOffline ? await promptly.prompt('Username:') : '';
+    const password = makeOffline ? await promptly.prompt('Password:') : '';
+
+    const template = (await fs.readFile('./template.html')).toString();
+    const html = generateHtml({ 
+        template, makeOffline, theme, tenantId, flowId, flowVersionId, username, password
     });
     
     mkdirp.sync('./www/css');
     mkdirp.sync('./www/css/themes');
     mkdirp.sync('./www/js');
 
-    await fs.writeFile('./www/css/mw-bootstrap.css', await rp.get('https://assets.manywho.com' + bundles.bootstrap3[0]));
-    await fs.writeFile('./www/css/ui-bootstrap.css', await rp.get('https://assets.manywho.com' + bundles.bootstrap3[1]));
-    await fs.writeFile('./www/js/ui-bootstrap.js', await rp.get('https://assets.manywho.com' + bundles.bootstrap3[2]));
-    await fs.writeFile('./www/js/ui-core.js', await rp.get('https://assets.manywho.com' + bundles.core[0]));
+    console.log('Downloading assets');
 
-    const theme = await promptly.prompt('Theme (defaults to paper):', {
-        default: 'paper'
-    });
+    await fs.writeFile('./www/css/mw-bootstrap.css', await rp.get('https://s3.amazonaws.com/manywho-cdn-react-staging' + bundles.bootstrap3[0]));
+    await fs.writeFile('./www/css/ui-bootstrap.css', await rp.get('https://s3.amazonaws.com/manywho-cdn-react-staging' + bundles.bootstrap3[1]));
+    await fs.writeFile('./www/js/ui-bootstrap.js', await rp.get('https://s3.amazonaws.com/manywho-cdn-react-staging' + bundles.bootstrap3[2]));
+    await fs.writeFile('./www/js/ui-core.js', await rp.get('https://s3.amazonaws.com/manywho-cdn-react-staging' + bundles.core[0]));
+    await fs.writeFile(`./www/css/themes/mw-${theme}.css`, await rp.get(`https://s3.amazonaws.com/manywho-cdn-react-staging/css/themes/mw-${theme}.css`));
 
-    await fs.writeFile(`./www/css/themes/mw-${theme}.css`, await rp.get('https://assets.manywho.com' + bundles.core[0]));
-
-    const tenantId = await promptly.prompt('Tenant Id:');
-    const flowId = await promptly.prompt('Flow Id:');
-    const flowVersionId = await promptly.prompt('Flow Version Id:', { default: '' });
-
-    const flowDetails = new RegExp('\/\/FLOWSTART.*\/\/FLOWEND', 'gs');
-
-    let html = (await fs.readFile('./www/index.html')).toString();
-    html = html.replace(flowDetails, `
-        //FLOWSTART
-        var tenantId = ${tenantId};
-        var flowId = ${flowId};
-        var flowVersionId = ${flowVersionId}
-        //FLOWEND
-    `);
+    if(makeOffline) {
+        const snapshot = await metadata({ username, password, tenantId, flowId, flowVersionId }).catch(e => console.log('Failed to get snapshot: ' + e));
+        await fs.writeFile('./www/js/ui-metadata.js', `window.flow.metadata = ${JSON.stringify(JSON.parse(snapshot))};\n`);
+        await fs.writeFile('./www/js/ui-offline.js', await rp.get('https://s3.amazonaws.com/manywho-cdn-react-staging' + bundles.offline[0]));
+    }
     
     console.log('Updating index.html');
+
     await fs.writeFile('./www/index.html', html);
+
+    process.exit();
 
     console.log('Done');
 })();
---
+++ metadata.js
@@ -0,0 +1,43 @@
+const rp = require('request-promise-native');
+
+module.exports = async ({ baseUrl = "https://staging.manywho.com", username, password, tenantId, flowId, flowVersionId }) => {
+
+    const token = await rp({
+        method: "POST",
+        uri: baseUrl + "/api/draw/1/authentication",
+        body: {
+            "loginUrl": baseUrl + "/plugins/manywho/api/draw/1/authentication",
+            "username": username,
+            "password": password
+        },
+        headers: {
+            'ManyWhoTenant': 'da497693-4d02-45db-bc08-8ea16d2ccbdf'
+        },
+        json: true
+    });
+
+    const authToken = await rp({
+        method: "GET",
+        uri: baseUrl + "/api/draw/1/authentication/" + tenantId,
+        headers: {
+            'authorization': token,
+        }
+    });
+
+    const authTokenClean = authToken.replace(/\"/g, '');
+
+    console.log('clean: ', authTokenClean)
+    console.log('uri: ', baseUrl + "/api/draw/1/flow/snap/" + flowId + "/" + flowVersionId)
+    console.log('tenantId: ', tenantId)
+
+    const snapshot = await rp({
+        method: "GET",
+        uri: baseUrl + "/api/draw/1/flow/snap/" + flowId + "/" + flowVersionId,
+        headers: {
+            'authorization': authTokenClean,
+            'ManyWhoTenant': tenantId
+        }
+    });
+
+    return snapshot;
+};
